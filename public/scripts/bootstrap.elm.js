24454197c1d7078d0ddce980e5f11879a47a9a07
Panagiotis Georgiadis <panagiwtis.geor@gmail.com>
2019-10-27 12:05:21 +0000

    Migrated project to using ParcelJs
    The project up until now was using a custom pipeline which was compiling
    sass and elm files and was serving them. This was a temp solution until
    something better and cleared could be found ( and until I got some free
    time on my hands ). Hence its now migrated onto using parceljs which
    seems to be doing the job!

--- public/scripts/bootstrap.elm.js
+++ public/scripts/bootstrap.elm.js
@@ -1,7 +1,9 @@
+import { Elm } from "../../src/Main.elm";
+
 /*
     Flags won't contain anything as of now
     but we might review this later on.
 */
 let flags = null;
 
-let app = Elm.Main.init(flags);
+let app = Elm.Main.init({ flags });
